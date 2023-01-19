112f463b8b30af1e3334695268f79b47163b71bc
xw22222 <askoiu323@naver.com>
2023-01-19 20:53:13 +0900

    URL main 수정

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("iot/", include('iot.urls')),
+    path("", RedirectView.as_view(url="/iot/", permanent=True)),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
