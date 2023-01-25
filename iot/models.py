d794d2dbb33b99bc4bf5e56aad3ff5757d7eb4df
xw22222 <askoiu323@naver.com>
2023-01-25 13:24:48 +0900

    민지님 디렉토리 수정

--- iot/models.py
+++ iot/models.py
@@ -4,4 +4,5 @@ from django.db import models
 # sec_file/년도/월/일 폴더에 사진이 저장될 예정
 class SecFile(models.Model):
     file_name = models.CharField(max_length=100)
-    sec_file = models.FileField(upload_to="sec_file/%Y/%m/%d/")
+    #sec_file = models.FileField(upload_to="sec_file/%Y/%m/%d/")
+    sec_file = models.FileField(upload_to="")
